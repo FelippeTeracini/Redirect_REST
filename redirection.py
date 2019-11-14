984cfc191f4e15b3e91c32f0d24ed501a03611fb
Felippe Nalin Teracini <felippent@al.insper.edu.br>
2019-11-14 14:38:55 -0300

    Add files via upload

--- redirection.py
+++ redirection.py
@@ -2,11 +2,14 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 import uvicorn
 import requests
+import sys
+import getopt
 import json
+
 app = FastAPI()
 
-tasks_dictionary = {}
-new_ip = 'http://101.193.193.10:5000'
+server_address = ''
+PORT = 0
 
 
 class Task(BaseModel):
@@ -17,36 +20,55 @@ class Task(BaseModel):
 
 @app.get("/")
 def read_root():
-    return requests.get(new_ip + '/').json()
+    return requests.get(server_address + '/').json()
 
 
 @app.get("/task")
 def read_tasks():
-    return requests.get(new_ip + '/task').json()
+    return requests.get(server_address + '/task').json()
 
 
 @app.post("/task")
 def create_task(task: Task):
-    return requests.post(new_ip + '/task',
+    return requests.post(server_address + '/task',
                          data=json.dumps(task))
 
 
 @app.get("/task/{task_id}")
 def read_task(task_id: int):
-    return requests.get(new_ip + '/task/' + task_id).json()
+    return requests.get(server_address + '/task/' + task_id).json()
 
 
 @app.put("/task/{task_id}")
 def update_task(task_id: int, task: Task):
-    return requests.post(new_ip + '/task/' + task_id,
+    return requests.post(server_address + '/task/' + task_id,
                          data=json.dumps(task))
 
 
 @app.delete("/task/{task_id}")
 def delete_task(task_id: int):
-    return requests.delete(new_ip + '/task/' + task_id).json()
+    return requests.delete(server_address + '/task/' + task_id).json()
 
 
 @app.get("/healthcheck")
 def read_health():
-    return requests.get(new_ip + '/healthcheck').json()
+    return requests.get(server_address + '/healthcheck').json()
+
+
+def main(argv):
+    try:
+        opts, args = getopt.getopt(
+            argv, "h", ["server_address=", "port="])
+    except getopt.GetoptError:
+        print(
+            'redirection.py --server_address <server-address> --port <port>')
+        sys.exit(2)
+
+    for opt, arg in opts:
+        if opt == '--server_address':
+            server_address = arg
+        elif opt == '--port':
+            PORT = arg
+
+    uvicorn.run("redirection:app", host=server_address,
+                port=PORT, log_level="info")
